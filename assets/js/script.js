ed70b8552e924f6b7c92c48bac7568b3f926a750
TaraHelberg <taralhelberg@outlook.com>
2022-06-24 13:56:25 +0000

    add js function to flip cards,modification still required

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,19 @@
 const cards = document.querySelectorAll('.flip-card');
 
+let hasFlippedCard = false;
+let firstCard,secondCard;
+
+//Flip Card Function
 function flipcard(){
-    console.log('I was clicked!')
-    console.log(this);
+    this.classList.add("flip"); 
+    
+    if(!hasFlippedCard) {
+
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipcard));
