fa23a2bd846e4ce5574f79fdfbd53abb1eb0be0f
TaraHelberg <taralhelberg@outlook.com>
2022-06-27 11:24:46 +0000

    add match up cards functions , 3 functions 1 for checking cards data-id matched , 2 if cards match disable and 3 if no match up board is locked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,24 @@
 const cards = document.querySelectorAll('.flip-card'); //For Flip Card Function
-                         
+const max_match = 8;                                   // max_match = 8 16 cards /2 as 1 flip count =2 flipped cards
 
 
 
-let hasFlippedCard = false; 
-let firstCard, secondCard; // fo card match check 
+
+let CardFlipped = false; 
+let firstCard, secondCard; // for card match check 
+let cardsMatched = 0;
+let lockBoard = false; // lock the game board until cards are matched       
 
 
- 
 
 //Flip Card Function
 function flipcard(){
 
     this.classList.add('flip');
 
-    if(!hasFlippedCard) {
+    if(!CardFlipped) {
 
-        hasFlippedCard = true;
+        CardFlipped = true;
         firstCard = this;
 
         return;
@@ -33,5 +35,37 @@ cards.forEach(card => {
     const ramdomPositn = Math.floor(Math.random() * cards.length); 
     card.style.order = ramdomPositn;    
 });
+
+// Used to check if firstCard and secondCard 'data-id = data-image as dataset' are matching up
+function matchedUp() {
+    let matched = firstCard.dataset.image === secondCard.dataset.image;
+    if(matched) cardsMatched += 1;
+
+    if(matched) twoMatched();
+    else noMatch();
+
+    if(cardsMatched === max_match) GameWon();
+} 
+
+// The two Matched cards will be disabled for clicks once flipped and matched 
+function twoMatched() {
+
+    firstCard.removeEventListener('click', flipcard);
+    secondCard.removeEventListener('click', flipcard);
+
+    resetGame();
+}
+
+// If no cards are Matched , game board is locked until cards are flipped Back
+function noMatch() {
+      lockBoard = true;
+      
+      setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetGame();
+      }, 800);     
+}
 
 
